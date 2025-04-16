27ef6e04cd4dc90fba7946ce879535a053b3119c
Zach Ullmeyer <zacharyullmeyer@gmail.com>
2025-04-15 20:29:15 -0400

    tweaked last commit

---
+++ src/ActivityTests.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import tools.Edge;
+import tools.Utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class ActivityTests {
+
+    @Test
+    public void test() {
+        //TODO: make tests
+    }
+
+}
